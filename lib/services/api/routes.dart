38db617e84450839cb6ea4cec4ba8633edfbcb37
ericaig <deleaighewi@hotmail.com>
2020-08-11 11:54:58 +0200

    Initial commit

--- lib/services/api/routes.dart
+++
@@ -1,5 +0,0 @@
-abstract class ApiRoutes {
-  static const String domain = 'https://example.com/mobile-api/';
-  static const String login = '/login';
-  static const String retrieveUser = '/retrieve-user';
-}
